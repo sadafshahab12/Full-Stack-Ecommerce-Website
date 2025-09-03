e7cb046d237b376d7c62fe299832b98750f4a105
sadafshahab12 <sadafshahab123@yahoo.com>
2025-09-03 13:48:49 +0500

    done

--- routes/protectedRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { verifyToken } from "../middleware/authMiddlerware";
-const protectedRouter = express.Router();
-
-protectedRouter.get("/dashboard", verifyToken, (req, res) => {
-  res.json({ message: "Welcome to Dashboard", user: req.user });
-});
-export { protectedRouter };
